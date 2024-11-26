2f0a9e9f7dca233e52da647c27f7c1acaa8a8d83
LucasMLK <lucasmlkop21@gmail.com>
2024-11-26 15:43:45 +0800

    update jmh test

--- src/test/java/io/xdag/crypto/randomx/RandomXJNAPerfTest.java
+++ src/test/java/io/xdag/crypto/randomx/RandomXJNAPerfTest.java
@@ -26,16 +26,8 @@ package io.xdag.crypto.randomx;
 import java.util.EnumSet;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Threads;
+
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
@@ -45,7 +37,8 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 @State(Scope.Benchmark)
 @Threads(value = 1)
 @BenchmarkMode({ Mode.Throughput, Mode.AverageTime})
-@Measurement(iterations = 1, time = 100)
+@Measurement(iterations = 5, time = 10)
+@Warmup(iterations = 1, time = 10)
 @OutputTimeUnit(TimeUnit.SECONDS)
 public class RandomXJNAPerfTest {
 
